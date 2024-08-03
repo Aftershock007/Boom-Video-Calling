7115fd2a843efd700b1a2b108f1327123b2933f4
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-08-04 04:56:41 +0530

    Meeting rooms added

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from "@/providers/StreamClientProvider"
+import { Metadata } from "next"
 import { ReactNode } from "react"
+
+export const metadata: Metadata = {
+  title: "Boom",
+  description: "Made with ❤️ by Arijit",
+  icons: {
+    icon: "/icons/logo.svg"
+  }
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
