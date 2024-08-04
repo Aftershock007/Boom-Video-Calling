3b2bffca9a9f0bcc46b238474e3df496130b24da
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-08-04 14:49:17 +0530

    small bug fixes and change the logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
           {children}
           <Toaster />
           <Analytics />
-          <SpeedInsights />
         </body>
       </ClerkProvider>
     </html>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,21 @@ import { SignedIn, UserButton } from "@clerk/nextjs"
 export default function Navbar() {
   return (
     <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-      <Link href="/" className="flex items-center gap-2">
+      <Link href="/" className="flex items-center justify-center gap-2">
         <Image
           src="/icons/logo.svg"
-          width={32}
-          height={32}
+          width={40}
+          height={40}
           alt="Boom Logo"
           className="max-sm:size-10"
         />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          Boom
-        </p>
+        <Image
+          src="/icons/boom-logo.svg"
+          width={75}
+          height={75}
+          alt="Boom Logo"
+          className="max-sm:size-10"
+        />
       </Link>
       <div className="flex-between gap-5">
         <SignedIn>
