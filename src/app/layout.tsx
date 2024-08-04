52eaa29386b1570d787a5dcacc8996232d5d404c
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-08-04 13:53:15 +0530

    added analytics and speedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { ClerkProvider } from "@clerk/nextjs"
 import { Toaster } from "@/components/ui/toaster"
 import "@stream-io/video-react-sdk/dist/css/styles.css"
 import "react-datepicker/dist/react-datepicker.css"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -40,6 +42,8 @@ export default function RootLayout({
         <body className={`${inter.className} bg-dark-2`}>
           {children}
           <Toaster />
+          <Analytics />
+          <SpeedInsights />
         </body>
       </ClerkProvider>
     </html>
