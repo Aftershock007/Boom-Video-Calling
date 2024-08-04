469728e01168c275a375486474d02b807c3aeae3
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-08-04 20:42:13 +0530

    fixed upcoming meetings

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
+import Loader from "@/components/Loader"
 import MeetingTypeList from "@/components/MeetingTypeList"
 import { useGetCalls } from "@/hooks/useGetCalls"
-import { Loader } from "lucide-react"
 
 export default function Home() {
   const now = new Date()
@@ -18,17 +18,37 @@ export default function Home() {
   )
 
   const { upcomingCalls, isLoading } = useGetCalls()
+  let upcomingMeetingTime = ""
 
-  if (!isLoading && upcomingCalls)
-    console.log(upcomingCalls[0]?.state?.startsAt?.toLocaleString())
+  if (!isLoading && upcomingCalls && upcomingCalls[0]?.state?.startsAt) {
+    const startsAt = new Date(upcomingCalls[0].state.startsAt)
+    const formattedDate = startsAt
+      .toLocaleDateString("en-US", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+      })
+      .replace(/,/g, "")
+    const formattedTime = startsAt.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true
+    })
+    upcomingMeetingTime = `${formattedDate}, ${formattedTime}`
+  }
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
-          </h2>
+          {upcomingMeetingTime ? (
+            <h2 className="glassmorphism max-w-[28.5rem] rounded py-2 text-center text-base font-normal">
+              Upcoming Meeting at: {upcomingMeetingTime}
+            </h2>
+          ) : (
+            <Loader />
+          )}
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
