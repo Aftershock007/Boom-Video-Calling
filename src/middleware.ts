6ee167a035a7c12f23ccf9c8605388aeb0f7bfb3
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-08-05 01:12:54 +0530

    bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,15 +10,14 @@ const protectedRoutes = createRouteMatcher([
 ])
 
 export default clerkMiddleware((auth, req) => {
-  if (protectedRoutes(req)) auth().protect()
+  if (protectedRoutes(req)) {
+    auth().protect()
+  }
 })
 
 export const config = {
   matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)"
   ]
 }
